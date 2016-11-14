b86561e35a7529b59ace82ca622d7610b5a92770
gracektay <gktaydesigns@gmail.com>
2016-11-14 17:09:47 -0500

    Added solution for module 4 assignment

---
+++ TayDebugFixIfStmt/TayDebugFixIfStmt/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugFixIFStmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+
+        void run()
+        {
+            int firstChoice = 0, secondChoice = 0, thirdChoice = 0;
+
+            System.Console.WriteLine("Grace Tay's Copy");
+
+            firstChoice = 0; secondChoice = 0; thirdChoice = 0;
+            WriteCurrentChoices(firstChoice, secondChoice, thirdChoice);
+
+            firstChoice = 2; secondChoice = 0; thirdChoice = 0;
+            WriteCurrentChoices(firstChoice, secondChoice, thirdChoice);
+
+            firstChoice = 2; secondChoice = 5; thirdChoice = 0;
+            WriteCurrentChoices(firstChoice, secondChoice, thirdChoice);
+
+            firstChoice = 2; secondChoice = 5; thirdChoice = 7;
+            WriteCurrentChoices(firstChoice, secondChoice, thirdChoice);
+        }
+
+
+        void WriteCurrentChoices(int firstChoice, int secondChoice, int thirdChoice)
+        {
+            if (firstChoice == 0)
+                Console.WriteLine("Choices are: {0}, {1}, {2} => There are no choices yet", firstChoice, secondChoice, thirdChoice);
+            else if (secondChoice == 0) //Error 2: added '=' since we need an equality operator
+                Console.WriteLine("Choices are: {0}, {1}, {2} => Currently choices are {0}", firstChoice, secondChoice, thirdChoice);  //Error 4: Removed the improperly placed first choice
+            else if (thirdChoice == 0) //Error 1: Deleted the extra equal sign
+                Console.WriteLine("Choices are: {0}, {1}, {2} => Currently choices are {0}, {1}", firstChoice, secondChoice, thirdChoice, firstChoice, secondChoice);
+            else if (thirdChoice == 7) //Error 3: changed 0 to 7 to match the fourth block of code  above, where third choice equals 7
+                Console.WriteLine("Choices are: {0}, {1}, {2} => Currently choices are {0}, {1}, {2}",
+                    firstChoice, secondChoice, thirdChoice, firstChoice, secondChoice, thirdChoice);
+        }
+    }
+}
+
---
+++ TayDebugFixIfStmt/TayDebugFixIfStmt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TayDebugFixIfStmt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SNHU")]
+[assembly: AssemblyProduct("TayDebugFixIfStmt")]
+[assembly: AssemblyCopyright("Copyright © SNHU 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94127782-91db-434a-a9ea-94830a20f2e0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
