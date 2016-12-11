c886810871f42e7f34409a5fb32093e62c9242e8
gracektay <grace.tay@snhu.edu>
2016-12-10 23:45:28 -0500

    Optimized version

--- Tay_grace_WPFRegisterStudent/WPFRegisterStudent/MainWindow.xaml.cs
+++ Tay_grace_WPFRegisterStudent/WPFRegisterStudent/MainWindow.xaml.cs
@@ -41,7 +41,6 @@ namespace WPFRegisterStudent
             Course course6 = new Course("IT 328");
             Course course7 = new Course("IT 330");
 
-
             this.comboBox.Items.Add(course1);
             this.comboBox.Items.Add(course2);
             this.comboBox.Items.Add(course3);
@@ -50,13 +49,12 @@ namespace WPFRegisterStudent
             this.comboBox.Items.Add(course6);
             this.comboBox.Items.Add(course7);
 
-
             this.textBox.Text = "";
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
-        { 
-            int numericalCourseIdentifier = 0;
+        {
+            /*int numericalCourseIdentifier = 0;
 
             choice = (Course)(this.comboBox.SelectedItem);
             string chosenCourseName = choice.ToString(); //Retrieves string of course name
@@ -91,6 +89,25 @@ namespace WPFRegisterStudent
 
                     break;
             }
+            choice = (Course)(this.comboBox.SelectedItem);
+            string chosenCourseName = choice.ToString(); //Retrieves string of course name
+
+            switch (validateSelection(choice))
+            {
+                case 1:
+                    label3.Content = "You have already registered for this " + chosenCourseName + " course.";
+                    break;
+                case 2:
+                    label3.Content = "You cannot register for more than 9 credit hours.";
+                    break;
+                case 0:
+                    label3.Content = "Registration confirmed for course " + chosenCourseName; //Updates label to confirm registration
+                    choice.SetToRegistered();
+                    TotalCredits += 3;
+                    textBox.Text = TotalCredits.ToString();
+                    listBox.Items.Add(choice); //Update the listBox with each registration
+                    break;
+            }
         }
 
         int ValidateUserSelection (int choice, int firstChoice, int secondChoice, int thirdChoice, int totalCredit)
@@ -102,40 +119,35 @@ namespace WPFRegisterStudent
             else if (totalCredit > 8)
                 return -3;
             return -4;
-        }
+        */
+            choice = (Course)(this.comboBox.SelectedItem);
+            string chosenCourseName = choice.ToString(); //Retrieves string of course name
 
-        int IdentifyCourse (string chosenCourseName, int numericalCourseIdentifier)
-        {
-            
-            switch (chosenCourseName) //Created switchstatement to see what course was selected and equate it to a numerical course identifier value
+            switch (ValidateUserSelection(choice))
             {
-                case "IT 145":
-                    numericalCourseIdentifier = 1;
-                break;
-                case "IT 200":
-                    numericalCourseIdentifier = 2;
-                break;
-                case "IT 201":
-                    numericalCourseIdentifier = 3;
-                break;
-                case "IT 270":
-                    numericalCourseIdentifier = 4;
-                break;
-                case "IT 315":
-                    numericalCourseIdentifier = 5;
-                break;
-                case "IT 328":
-                    numericalCourseIdentifier = 6;
-                break;
-                case "IT 330":
-                    numericalCourseIdentifier = 7;
-                break;
-                default:
-                    numericalCourseIdentifier = 0;
-                break;
+                case 1:
+                    label3.Content = "You have already registered for this " + chosenCourseName + " course.";
+                    break;
+                case 2:
+                    label3.Content = "You cannot register for more than 9 credit hours.";
+                    break;
+                case 0:
+                    label3.Content = "Registration confirmed for course " + chosenCourseName; //Updates label to confirm registration
+                    choice.SetToRegistered();
+                    TotalCredits += 3;
+                    textBox.Text = TotalCredits.ToString();
+                    listBox.Items.Add(choice); //Update the listBox with each registration
+                    break;
             }
 
-            return numericalCourseIdentifier;
+        }
+        int ValidateUserSelection(Course selectedCourse)
+        {
+            if (selectedCourse.IsRegisteredAlready())
+                return 1;
+            else if (TotalCredits > 8)
+                return 2;
+            return 0;
         }
     }
 }
